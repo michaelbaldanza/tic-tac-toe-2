cbdc920e77a99915f72f0667f9355437d8e122e7
Michael Baldanza <michael.snider.baldanza@gmail.com>
2020-11-21 14:37:24 -0500

    refactor DOM manipulation to reduce lines of code

--- js/main.js
+++ js/main.js
@@ -18,32 +18,22 @@ let xScore = 0;
 let oScore = 0;
 
 /*----- cached element references -----*/
-const topLeftEl = document.getElementById('top-left');
-const topMiddleEl = document.getElementById('top-middle');
-const topRightEl = document.getElementById('top-right');
-const centerLeftEl = document.getElementById('center-left');
-const centerMiddleEl = document.getElementById('center-middle');
-const centerRightEl = document.getElementById('center-right');
-const bottomLeftEl = document.getElementById('bottom-left');
-const bottomMiddleEl = document.getElementById('bottom-middle');
-const bottomRightEl = document.getElementById('bottom-right');
 
-const messageEl = document.getElementById('message');
+// get array of board squares
+const squareEls = document.querySelectorAll('div.square');
+
 const resetBtn = document.getElementById('reset-btn');
 
+const messageEl = document.getElementById('message');
 const xScoreEl = document.getElementById('x-score');
 const oScoreEl = document.getElementById('o-score');
 
 /*----- event listeners -----*/
-topLeftEl.addEventListener('click', takeTurn);
-topMiddleEl.addEventListener('click', takeTurn);
-topRightEl.addEventListener('click', takeTurn);
-centerLeftEl.addEventListener('click', takeTurn);
-centerMiddleEl.addEventListener('click', takeTurn);
-centerRightEl.addEventListener('click', takeTurn);
-bottomLeftEl.addEventListener('click', takeTurn);
-bottomMiddleEl.addEventListener('click', takeTurn);
-bottomRightEl.addEventListener('click', takeTurn);
+
+// add event listeners to squares
+squareEls.forEach(function (squareEl) {
+  squareEl.addEventListener('click', takeTurn);
+});
 
 resetBtn.addEventListener('click', reset);
 
@@ -150,76 +140,29 @@ function reset() {
 }
 
 function eventListenerReset() {
-  if (topLeftEl.getAttribute('listener') !== 'true') {
-    topLeftEl.addEventListener('click', takeTurn);
-    topLeftEl.setAttribute('listener','true');
-  }
-  if (topMiddleEl.getAttribute('listener') !== 'true') {
-    topMiddleEl.addEventListener('click', takeTurn);
-    topMiddleEl.setAttribute('listener','true');
-  }
-  if (topRightEl.getAttribute('listener') !== 'true') {
-    topRightEl.addEventListener('click', takeTurn);
-    topRightEl.setAttribute('listener','true');
-  }
-  if (centerLeftEl.getAttribute('listener') !== 'true') {
-    centerLeftEl.addEventListener('click', takeTurn);
-    centerLeftEl.setAttribute('listener','true');
-  }
-  if (centerMiddleEl.getAttribute('listener') !== 'true') {
-    centerMiddleEl.addEventListener('click', takeTurn);
-    centerMiddleEl.setAttribute('listener','true');
-  }
-  if (centerRightEl.getAttribute('listener') !== 'true') {
-    centerRightEl.addEventListener('click', takeTurn);
-    topLeftEl.setAttribute('listener','true');
-  }
-  if (bottomLeftEl.getAttribute('listener') !== 'true') {
-    bottomLeftEl.addEventListener('click', takeTurn);
-    bottomLeftEl.setAttribute('listener','true');
-  }
-  if (bottomMiddleEl.getAttribute('listener') !== 'true') {
-    bottomMiddleEl.addEventListener('click', takeTurn);
-    bottomMiddleEl.setAttribute('listener','true');
-  }
-  if (bottomRightEl.getAttribute('listener') !== 'true') {
-    bottomRightEl.addEventListener('click', takeTurn);
-    bottomRightEl.setAttribute('listener','true');
-  }
+  // add event listeners back
+  squareEls.forEach(function(squareEl) {
+    if (squareEl.getAttribute('listener') !== 'true') {
+      squareEl.addEventListener('click', takeTurn);
+      squareEl.setAttribute('listener', 'true');
+    }
+  });
 }
 
 function boardReset() {
-  topLeftEl.textContent = '';
-  topMiddleEl.textContent = '';
-  topRightEl.textContent = '';
-  centerLeftEl.textContent = '';
-  centerMiddleEl.textContent = '';
-  centerRightEl.textContent = '';
-  bottomLeftEl.textContent = '';
-  bottomMiddleEl.textContent = '';
-  bottomRightEl.textContent = '';
+  // clear X's and O's from the board
+  squareEls.forEach(function(squareEl) {
+    squareEl.textContent = '';
+  });
 }
 
 function rmvEvtListeners() {
-  topLeftEl.removeEventListener('click', takeTurn);
-  topMiddleEl.removeEventListener('click', takeTurn);
-  topRightEl.removeEventListener('click', takeTurn);
-  centerLeftEl.removeEventListener('click', takeTurn);
-  centerMiddleEl.removeEventListener('click', takeTurn);
-  centerRightEl.removeEventListener('click', takeTurn);
-  bottomLeftEl.removeEventListener('click', takeTurn);
-  bottomMiddleEl.removeEventListener('click', takeTurn);
-  bottomRightEl.removeEventListener('click', takeTurn);
-
-  topLeftEl.setAttribute('listener','false');
-  topMiddleEl.setAttribute('listener','false');
-  topRightEl.setAttribute('listener','false');
-  centerLeftEl.setAttribute('listener','false');
-  centerMiddleEl.setAttribute('listener','false');
-  centerRightEl.setAttribute('listener','false');
-  bottomLeftEl.setAttribute('listener','false');
-  bottomMiddleEl.setAttribute('listener','false');
-  bottomRightEl.setAttribute('listener','false');
+  squareEls.forEach(function(squareEl) {
+    squareEl.removeEventListener('click', takeTurn);
+  });
+  squareEls.forEach(function(squareEl) {
+    squareEl.setAttribute('listener', 'false');
+  });
 }
 
 function checkTieCondition() {
